7c010cd100228789db4ea0197678f0ad06e751f3
rumeysakdogan <dgn.rumeysaa@gmail.com>
2022-10-13 22:42:12 -0400

    adding bash script

---
+++ DevOps-Capstone-Project-Microservices-CI:CD-Pipeline/msp-23-rancher-tf-files/rancherdata.sh
@@ -0,0 +1,13 @@
+#! /bin/bash
+hostnamectl set-hostname rancher-instance-1
+apt-get update -y
+apt-get upgrade -y
+apt-get install apt-transport-https ca-certificates curl gnupg lsb-release -y
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
+echo "deb [arch=amd64 signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+apt-get update
+apt-get install docker-ce docker-ce-cli containerd.io -y
+systemctl start docker
+systemctl enable docker
+usermod -aG docker ubuntu
+newgrp docker
