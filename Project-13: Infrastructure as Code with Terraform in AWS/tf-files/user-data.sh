ec5df1d9209d30bc310b9447a1277a2aaf9ef5bb
rumeysakdogan <dgn.rumeysaa@gmail.com>
2022-12-08 01:51:40 -0500

    updated user-data.sh

--- Project-13: Infrastructure as Code with Terraform in AWS/tf-files/user-data.sh
+++ Project-13: Infrastructure as Code with Terraform in AWS/tf-files/user-data.sh
@@ -5,5 +5,5 @@ pip3 install flask
 pip3 install flask_mysql
 yum install git -y
 TOKEN="XXXXXXXXXXXXXXXXXXXXX"
-cd /home/ec2-user && git clone https://$TOKEN@github.com/rumeysakdogan/phonebook.git
+cd /home/ec2-user && git clone https://$TOKEN@github.com/<your_github_account_name>/phonebook.git
 python3 /home/ec2-user/phonebook/phonebook-app.py
