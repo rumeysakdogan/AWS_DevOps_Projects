ebb3369798061b48e219e94c18bb8cccc4f54018
rumeysakdogan <dgn.rumeysaa@gmail.com>
2022-12-11 01:02:31 -0500

    adding project-15

---
+++ Project-15_Deploying MS Architecture with Kubernetes /terraform-files-to-create-K8s-cluster/master.sh
@@ -0,0 +1,38 @@
+#! /bin/bash
+apt-get update -y
+apt-get upgrade -y
+hostnamectl set-hostname kube-master
+chmod 777 /etc/sysctl.conf
+echo "net.ipv4.ip_forward=1" >> /etc/sysctl.conf
+sysctl -p
+chmod 644 /etc/sysctl.conf
+apt install -y docker.io
+systemctl start docker
+mkdir /etc/docker
+cat <<EOF | tee /etc/docker/daemon.json
+{
+  "exec-opts": ["native.cgroupdriver=systemd"],
+  "log-driver": "json-file",
+  "log-opts": {
+    "max-size": "100m"
+  },
+  "storage-driver": "overlay2"
+}
+EOF
+systemctl enable docker
+sudo systemctl daemon-reload
+sudo systemctl restart docker
+usermod -aG docker ubuntu
+newgrp docker
+apt install -y apt-transport-https
+curl -s https://packages.cloud.google.com/apt/doc/apt-key.gpg | sudo apt-key add -
+apt-add-repository "deb http://apt.kubernetes.io/ kubernetes-xenial main"
+apt update
+apt install -y kubelet=1.25.0-00 kubeadm=1.25.0-00 kubectl=1.25.0-00
+systemctl start kubelet
+systemctl enable kubelet
+kubeadm init --pod-network-cidr=172.16.0.0/16 --ignore-preflight-errors=All
+mkdir -p /home/ubuntu/.kube
+cp -i /etc/kubernetes/admin.conf /home/ubuntu/.kube/config
+chown ubuntu:ubuntu /home/ubuntu/.kube/config
+su - ubuntu -c 'kubectl apply -f https://docs.projectcalico.org/manifests/calico.yaml'
