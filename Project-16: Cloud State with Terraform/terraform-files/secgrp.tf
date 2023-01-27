f1418a8a1cb1d3e72da06506e26b66600e2d0a2a
Osomudeya Zudonu <talk2zudonuosomudeya@gmail.com>
2023-01-27 01:26:25 +0100

    Update secgrp.tf

--- Project-16: Cloud State with Terraform/terraform-files/secgrp.tf
+++ Project-16: Cloud State with Terraform/terraform-files/secgrp.tf
@@ -30,7 +30,7 @@ resource "aws_security_group" "vprofile-bastion-sg" {
     from_port   = 22
     protocol    = "tcp"
     to_port     = 22
-    cidr_blocks = [var.MyIP]
+    cidr_blocks = ["0.0.0.0/0"]
   }
 }
 
